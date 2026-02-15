267288b286bc429fbc99d6ff4e10db06ada8692b
Himanshu Singh <hsperfect1111@gmail.com>
2026-02-15 15:07:28 +0530

    23-passing-children

---
+++ 23-passing-children/src/App.jsx
@@ -0,0 +1,29 @@
+import FoodItems from "./components/FoodItems";
+import ErrorMessage from "./components/ErrorMessage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Container from "./components/Container";
+
+function App() {
+  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  //   let foodItems = [];
+
+  return (
+    <>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+
+      <Container>
+        <p>
+          Above is the list of healthy foods that are good for your health and
+          well being
+        </p>
+      </Container>
+    </>
+  );
+}
+
+export default App;
